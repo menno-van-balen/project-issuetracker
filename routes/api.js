5aaaf8b7df0fcd090120918a324ee4fe1bad8e71
Menno van Balen <mennovanbalen@gmail.com>
2020-10-14 18:53:31 +0100

    added delete by id functionality

--- routes/api.js
+++ routes/api.js
@@ -21,13 +21,24 @@ module.exports = function (app) {
 
     .get(function (req, res) {
       var project = req.params.project;
+      console.log(req.params);
+      console.log(req.query);
+      //  possible variables in query
+      const {
+        _id,
+        issue_title,
+        issue_text,
+        created_on,
+        created_by,
+        open,
+        assigned_to,
+        status_text,
+      } = req.query;
+      console.log(open, assigned_to);
     })
 
     .post(function (req, res) {
       // variables for new issue
-      const _id = new ObjectId();
-      const created_on = new Date();
-      const open = true;
       const {
         issue_title,
         issue_text,
@@ -38,12 +49,12 @@ module.exports = function (app) {
 
       // create a new issue
       const issue = new Issue({
-        _id,
+        _id: new ObjectId(),
         issue_title,
         issue_text,
-        created_on,
+        created_on: new Date(),
         created_by,
-        open,
+        open: true,
         assigned_to,
         status_text,
       });
@@ -91,53 +102,70 @@ module.exports = function (app) {
       const update = {
         issue_title: (issue_title = issue_title[0] ? issue_title : undefined),
         issue_text: (issue_text = issue_text[0] ? issue_text : undefined),
+        updated_on: new Date(),
         created_by: (created_by = created_by[0] ? created_by : undefined),
         status_text: (status_text = status_text[0] ? status_text : undefined),
         open: open ? false : true,
       };
 
-      // only if id is in database update the issue
+      // if no fields are sent
+      if (
+        issue_title === undefined &&
+        issue_text === undefined &&
+        created_by === undefined &&
+        status_text === undefined &&
+        open === true
+      ) {
+        res.json("no updated field sent");
+      } else {
+        // only if id is in database update the issue
+        Issue.findById(_id)
+          .then((doc) => {
+            if (doc === null) {
+              res.json(`could not update ${_id}`);
+            } else {
+              Issue.findByIdAndUpdate(
+                _id,
+                update,
+                { new: true, omitUndefined: true },
+                (err, doc) => {
+                  if (err) {
+                    console.error(er);
+                    res.json(`could not update ${_id}`);
+                  } else {
+                    res.json("successfully updated");
+                  }
+                }
+              );
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            res.json(`could not update ${_id}`);
+          });
+      }
+    })
+
+    .delete(function (req, res) {
+      const _id = req.body._id;
+
       Issue.findById(_id)
         .then((doc) => {
           if (doc === null) {
-            res.json("unkown issue id");
+            res.json(`_id error`);
           } else {
-            Issue.findByIdAndUpdate(
-              _id,
-              update,
-              { new: true, omitUndefined: true },
-              (err, doc) => {
-                if (err) {
-                  console.error(er);
-                } else {
-                  const {
-                    _id,
-                    issue_title,
-                    issue_text,
-                    created_by,
-                    status_text,
-                    open,
-                  } = doc;
-
-                  res.json({
-                    _id,
-                    issue_title,
-                    issue_text,
-                    created_by,
-                    status_text,
-                    open,
-                  });
-                }
+            Issue.deleteOne({ _id }, (err) => {
+              if (err) {
+                console.error(err);
+                res.json(`could not delete ${_id}`);
               }
-            );
+              res.json(`deleted ${_id}`);
+            });
           }
         })
         .catch((err) => {
           console.error(err);
+          res.json(`could not delete ${_id}`);
         });
-    })
-
-    .delete(function (req, res) {
-      var project = req.params.project;
     });
 };
